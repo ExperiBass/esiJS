ba21663d405326cddab0d909302b0475144195b9
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2019-06-29 02:53:02 -0400

    Initial commit

---
+++ Functions/search.js
@@ -0,0 +1,79 @@
+module.exports = search
+
+function search(string, category, strict) {
+    let axios = require('axios')
+    let link = require('../esi.json').link
+    let settings = require('../settings.json')
+    let data;
+    let returningData;
+    let types = [
+                'agent',
+                'alliance',
+                'character',
+                'constellation',
+                'corporation',
+                'faction',
+                'inventory_type',
+                'region',
+                'solar_system',
+                'station'
+                ]
+  
+    if (typeof string === 'number') {
+        console.error(`The first argument MUST be a string!`)
+        return
+    }
+    if (typeof category) {
+        let s = category.toString().toLowerCase()
+        switch(s) {
+            case 'agent':
+                break;
+            case 'alliance':
+                break;
+            case 'character':
+                break;
+            case 'constellation':
+                break;
+            case 'corporation':
+                break;
+            case 'faction':
+                break;
+            case 'inventory_type':
+                break;
+            case 'region':
+                break;
+            case 'solar_system':
+                break;
+            case 'station':
+                break;
+            default:
+                console.error(`The second argument must be of one of the following types: ${types}`)
+                return;
+        }
+    }
+    if (strict == undefined) {
+        strict = false
+    }
+    if (typeof strict !== 'boolean') {
+        console.error(`The third parameter must be a boolean value!`)
+        return;
+    } 
+
+  await axios.get(`${link}search/?categories=${category}&datasource=tranquility&language=en-us&search=${string}&strict=${strict}`)
+        .then(response => {
+            if (response.statusText != 'OK') {
+                console.error('ERROR: Invalid search string')
+                return true
+            }
+            data = response.data
+
+        })
+    return data;
+}
+
+console.log(a())
+console.log(search('tritanium', 'inventory_type', true))
+function a() {
+    let info = search('tritanium', 'inventory_type', true)
+    console.log(info)
+}
