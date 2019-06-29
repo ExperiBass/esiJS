6a96e8695d68cd175d783d79fd29f3e8305e4b8b
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2019-06-29 03:06:06 -0400

    broken

--- Functions/search.js
+++ Functions/search.js
@@ -4,7 +4,7 @@ function search(string, category, strict) {
     let axios = require('axios')
     let link = require('../esi.json').link
     let settings = require('../settings.json')
-    let data;
+   // let data;
     let returningData;
     let types = [
                 'agent',
@@ -59,21 +59,26 @@ function search(string, category, strict) {
         return;
     } 
 
-  await axios.get(`${link}search/?categories=${category}&datasource=tranquility&language=en-us&search=${string}&strict=${strict}`)
+  let data = axios.get(`${link}search/?categories=${category}&datasource=tranquility&language=en-us&search=${string}&strict=${strict}`)
         .then(response => {
             if (response.statusText != 'OK') {
                 console.error('ERROR: Invalid search string')
                 return true
             }
             data = response.data
+            //console.log(returningData)
 
         })
-    return data;
+        
+        returningData = Promise.resolve(data)
+        console.log(data, 'line 73')
+    return returningData;
 }
 
 console.log(a())
-console.log(search('tritanium', 'inventory_type', true))
+//console.log(search('tritanium', 'inventory_type', true))
 function a() {
     let info = search('tritanium', 'inventory_type', true)
-    console.log(info)
+    console.log(info, 'line 80')
+    //console.log(info.inventory_type, 'line 81')
 }
