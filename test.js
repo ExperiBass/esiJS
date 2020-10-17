c136f9d36da66ecaa6392c42382c1705cabef40a
GingkathFox <gingkathfox@gmail.com>
2020-10-16 21:00:54 -0400

    version bump

--- test.js
+++ test.js
@@ -4,9 +4,5 @@ async function myFunc() {
     let client = new esiJS({
         logging: false
     })
-    let path = await client.search.search('The PathOwOgen', 'character', true)
-    path = path.data.character[0]
-    console.log(path)
-    let data = await client.character.medals(path)
 }
 myFunc().catch(e => console.error(e))
